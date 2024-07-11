ff30e51701db5d745185e7fd6dbd81364c985bf5
Chris Collis <theotteryears@me.com>
2024-07-11 15:37:38 +0100

    Layout practice

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,68 @@
 export default function App() {
   return (
-    <h1 className="text-3xl text-center">
-      Hello world!
-    </h1>
-  )
+    <div className="grid grid-cols-6">
+      <h1 className="col-span-4 col-start-2 m-3 rounded-md border-4 border-black bg-yellow-100 p-4 text-center text-4xl text-black">
+        LAYOUT TEST
+      </h1>
+      <h2 className="col-span-2 col-start-1 m-3 rounded-md border-4 border-solid border-pink-600 bg-pink-400 p-3 text-center text-3xl text-white">
+        Left
+      </h2>
+      <h2 className="col-span-2 col-start-3 m-3 rounded-md border-4 border-solid border-green-600 bg-green-400 p-3 text-center text-3xl text-white">
+        Center
+      </h2>
+      <h2 className="col-span-2 col-start-5 m-3 rounded-md border-4 border-solid border-indigo-600 bg-indigo-400 p-3 text-center text-3xl text-white">
+        Right
+      </h2>
+      <p className="col-span-1 col-start-1 m-3 rounded-md border-2 border-gray-300 p-3 text-center">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis,
+        inventore. Rerum quod debitis accusamus velit odio sed earum voluptas
+        facere laudantium voluptates eaque voluptatem animi eligendi doloremque
+        necessitatibus, quisquam quia.
+      </p>
+      <p className="col-span-1 col-start-2 m-3 p-3 text-center">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aspernatur,
+        provident, recusandae nemo fuga accusantium dolor nulla impedit
+        inventore numquam a maiores? Dolores eaque corrupti enim voluptas ad
+        delectus ipsam libero.
+      </p>
+      <p className="col-span-2 col-start-3 m-3 rounded-md border-2 border-gray-300 p-3 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat porro
+        quos laborum impedit. Atque, officia excepturi? Provident vitae, magnam
+        consequuntur, harum eaque officia aliquam earum nihil hic quaerat,
+        ducimus quam.
+      </p>
+      <p className="col-span-1 col-start-5 m-3 p-3 text-center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
+        accusantium necessitatibus officia quam cum. Tempore nisi voluptates
+        veniam quae. Doloribus, inventore repudiandae atque voluptatum rerum
+        quos at ipsam dolorem culpa!
+      </p>
+      <p className="col-span-1 col-start-6 m-3 rounded-md border-2 border-gray-300 p-3 text-center">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        pariatur eligendi omnis! Soluta quis culpa iure eaque reiciendis cumque,
+        doloremque qui voluptas doloribus natus eius vero totam dolorem facere
+        deleniti.
+      </p>
+      <p className="col-span-1 col-start-1 m-3 rounded-md border-2 border-solid border-black bg-pink-200 p-3 text-center text-black">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        pariatur eligendi omnis!
+      </p>
+      <p className="col-span-4 col-start-2 m-3 rounded-md border-2 border-solid border-black bg-green-200 p-3 text-center text-black">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        pariatur eligendi omnis! Soluta quis culpa iure eaque reiciendis cumque,
+        doloremque qui voluptas doloribus natus eius vero totam dolorem facere
+        deleniti. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+        totam consectetur, vero repudiandae nesciunt soluta iste ea dignissimos
+        inventore, tenetur eaque dicta recusandae perspiciatis qui! Vel
+        consequuntur necessitatibus consequatur optio! Lorem ipsum dolor sit,
+        amet consectetur adipisicing elit. Dolor numquam ipsum ut, provident ab
+        suscipit sequi dolore voluptates est obcaecati deserunt consequatur
+        itaque! Molestiae, dolore doloremque ex nemo illo obcaecati.
+      </p>
+      <p className="col-span-1 col-start-6 m-3 rounded-md border-2 border-solid border-black bg-indigo-200 p-3 text-center text-black">
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis
+        pariatur eligendi omnis!
+      </p>
+    </div>
+  );
 }
